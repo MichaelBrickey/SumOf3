6ef061f98fbcf679c1597eae8af46672894b0377
Michael Brickey <John.M.Brickey-1@ou.edu>
2021-02-05 14:10:32 -0600

    idk at this point

---
+++ SumOf3/SumOf3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+/** Michael Brickey**/
+namespace SumOf3 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Number1, Number2, Number3;
+           const double MAGIC_NUMBER = 7.777; //local constant variable
+            double total; 
+
+
+
+            Console.WriteLine("Please enter a whole number >>");
+            Number1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter a number with a decimal to the hundredths place >>");
+            Number2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter a number with a decimal to the thousandths place >>");
+            Number3 = Console.ReadLine();
+
+           total = Convert.ToDouble()
+
+
+
+            String output = Number1 + Number2 + Number3;
+            Console.WriteLine(output);
+
+            
+
+
+
+
+
+        }
+    }
+}
